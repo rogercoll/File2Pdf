4074a1688d75f2325fbd570bb23f0ac7ef2f5c7f
Roger Coll Aumatell <33873530+rogercoll@users.noreply.github.com>
2017-12-05 19:11:05 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -9,59 +9,86 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', function(req, res){
+  console.log(__dirname);
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+app.get('/download/:uid', function(req, res){
+  console.log(req.params.uid);
+  var aux = "uploads/" + req.params.uid;
+  var file = (path.join(__dirname, aux));
+  res.download(file);
+});
+
+
 app.post('/upload', function(req, res){
 
-  // create an incoming form object
   var form = new formidable.IncomingForm();
-
-  // specify that we want to allow the user to upload multiple files in a single request
   form.multiples = true;
-
-  // store all uploads in the /uploads directory
   form.uploadDir = path.join(__dirname, '/uploads');
-
-  // every time a file has been uploaded successfully,
-  // rename it to it's orignal name
   form.on('file', function(field, file) {
     fs.rename(file.path, path.join(form.uploadDir, file.name));
   });
-
-  // log any errors that occur
   form.on('error', function(err) {
     console.log('An error has occured: \n' + err);
   });
-
-  // once all the files have been uploaded, send a response to the client
   form.on('end', function() {
     res.end('success');
   });
-
-  // parse the incoming request containing the form data
   form.parse(req);
 
 });
 
+var isPaused = true;
+
+function os_func(){
+    isPaused = false;
+    this.execCommand = function(cmd,callback){
+    exec(cmd, (error,stdout,stderr) => {
+      if(error){
+        console.log(error);
+        return;
+      }
+      else{
+        isPaused = true;
+        callback(stdout);
+      }
+    });
+  }
+}
+
+
 app.post('/convert', function(req,res){
+
   var data = req.body;
   var com = "abiword --to=pdf ";
+  var files = '"files" : ';
+  var os;
   for(var i = 0; i < data.fitxers.length; ++i){
     var fname = data.fitxers[i].Namefile;
-    	var auxiliar = com + "uploads/" + fname;
-	exec(auxiliar, function(err, stdout, stderr){
-		if(err){
-			console.log(err);
-		}
-		else{
-			console.log("Conversion of " + fname + " succesful");
-
-		}
-	});
-	console.log(fname);
+    var auxiliar = com + "uploads/" + fname;
+    var substr = fname.substr(0, fname.indexOf('.'));
+    files += '"' + substr + '"';
+    if(i < data.fitxers.length -1) files += " , ";
+    os = new os_func();
+    os.execCommand(auxiliar,function(ret){
+      console.log("finished");
+    })
+	  console.log(fname);
   }
+  function wait4it(){
+    if(isPaused == true){
+      var txt = '{"msg":"redirect" , "location":"/download" , ' + files + "}" ;
+      console.log(txt);
+      res.end(txt);
+    }
+    else{
+      setTimeout(function(){wait4it()},100);
+    }
+  }
+  wait4it();
 });
 
 
